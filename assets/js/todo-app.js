77b3483d7a08e3b3679ce1506a36164ff124d69a
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2017-11-06 17:50:19 -0600

    added the logic to fadeout new list items entered

--- assets/js/todo-app.js
+++ assets/js/todo-app.js
@@ -1,12 +1,24 @@
     //mark completed to-dos with a strike through
-    $("li").click(function (){
+    //code will affect current & future Li's to be added , hence the additional  "li"
+    $("ul").on("click", "li" ,function (){
     $(this).toggleClass("completed");
 });
 
 //delete the completed to-dos
-$(".x-button").on("click",function(e){
+$("ul").on("click", ".x-button",function(e){
     $(this).parent().fadeOut(500,function(){
     $(this).remove();
     }); 
     e.stopPropagation();
+});
+
+//add code to add new skills to list
+$("input[type='text']").on("keypress", function(e){
+    if(e.which===13){
+        //grab new toDo text from input box
+        var todoText = $(this).val();
+        $(this).val("");
+        //create new "li" and append it to our parent "ul"
+        $("ul").append("<li><span class='x-button'>X </span>" +todoText+"</li>");
+    }
 });
