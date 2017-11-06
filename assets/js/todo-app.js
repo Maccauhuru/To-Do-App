7a76ddc4eec9a840c903277041017136596ad2df
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2017-11-06 17:04:46 -0600

    added code to delete the completed items

--- assets/js/todo-app.js
+++ assets/js/todo-app.js
@@ -2,3 +2,11 @@
     $("li").click(function (){
     $(this).toggleClass("completed");
 });
+
+//delete the completed to-dos
+$(".x-button").on("click",function(e){
+    $(this).parent().fadeOut(500,function(){
+    $(this).remove();
+    }); 
+    e.stopPropagation();
+});
